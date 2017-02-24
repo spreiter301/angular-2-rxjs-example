a2dec4063fea6bd01e1da3b5525f948c588618e5
Heinrich Spreiter <spreiter301@gmail.com>
2017-02-24 20:00:34 +0100

    example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, Subject, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  list: Observable<string[]>
+  search = new FormControl();
+
+
+  ngOnInit() {
+    this.list = this.search.valueChanges
+      .filter(value => value.length > 0)
+      .debounceTime(500)
+      .switchMap(this.fakeHttpRequest);
+  }
+
+  fakeHttpRequest(value) {
+    return Observable.create((obs: Observer<string[]>) => {
+      setTimeout(() => {
+        console.log("make request " + value);
+        obs.next([value + " one", value + " two", value + " three"]);
+        obs.complete();
+      }, 1000);
+    });
+
+  }
+
 }
